1679b638dd591ff8aac369b101c43b722d268c4f
faizanahmed-devstree <faizanahmed.devstree>
2024-05-08 10:11:39 +0530

    added oops

---
+++ OOPS/tut1.js
@@ -0,0 +1,17 @@
+class Student {
+  constructor(name, techStack, college, degree) {
+    this.name = name;
+    this.techStack = techStack;
+    this.college = college;
+    this.degree = degree;
+  }
+}
+
+const faizan = new Student(
+  "Faizan",
+  ["Next", "React", "Ionic"],
+  "Nemon International School Of Technology",
+  "Btech in Computer Science and Data Science"
+);
+
+console.log(faizan.techStack);
